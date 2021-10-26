036680c061d1e6a7bcf4205489a2b02556eaec48
0xean <0xean.eth@gmail.com>
2021-10-26 07:28:41 -0600

    basic test framework working

--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-import SDK from '../src';
-const { assert } = chai;
-
-describe('SDK', () => {
-  it('Can be created via constructor', async () => {
-    const sdk = new SDK({});
-  });
-});
