536e8619503f05819a64a36d953b3122c6ab97b8
0xean <0xean.eth@gmail.com>
2021-10-27 16:58:10 -0600

    deployments working with HH

---
+++ test/exchange/ExchangeFactory.test.mjs
@@ -0,0 +1,23 @@
+/* eslint import/extensions: 0 */
+import chai from 'chai';
+import fetch from 'node-fetch';
+import elasticSwapSDK from '../../dist/index.js';
+import hardhat from 'hardhat';
+
+const { ethers, deployments } = hardhat;
+const { assert } = chai;
+const RPC_URL = 'https://mainnet.infura.io/v3/48f877fa4aa4490bb0c988368dc8e373';
+
+describe('ExchangeFactory', () => {
+  it('Does something', async () => {
+    // accounts = await ethers.getSigners();
+    // [, , , , deployer] = accounts;
+    await deployments.fixture();
+    // const ExchangeFactory = await deployments.get("ExchangeFactory");
+    // exchangeFactory = new ethers.Contract(
+    //   ExchangeFactory.address,
+    //   ExchangeFactory.abi,
+    //   accounts[0]
+    // );
+  });
+});
