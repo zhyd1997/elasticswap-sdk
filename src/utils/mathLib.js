293d57c4d93f66687ed9e740aab712c68bf159e7
SSDD <smalldutta@gmail.com>
2021-11-04 18:10:53 +0200

    refactors test

--- src/utils/mathLib.js
+++ src/utils/mathLib.js
@@ -6,30 +6,25 @@ const {utils} = sdk;
 // calculates the min amount of output tokens given the slippage percent supplied
 const calculateOutputAmount = (inputTokenAmount, inputTokenReserveQty, outputTokenReserveQty, slippagePercent) => {
 
-  // got to make sure inputs come  in clean -BN
   // inputTokenReserveQty - X
-
   // logic: K = X*Y
   // outputTokenAmount = ((X*Y)/ inputTokenAmount)* (1 - (slippagePercent/100))
-  // max outputtoken = (X*Y)/ inputTokenAmount)
-  // min output token = (X*Y)/ inputTokenAmount) * (1 - (maxSlippage/100))
   // cleanse input to BN
+  const inputTokenAmountBN = BigNumber(inputTokenAmount);
+  const inputTokenReserveQtyBN = BigNumber(inputTokenReserveQty);
+  console.log('inputTokenReserveQtyBN.toString()', inputTokenReserveQtyBN.toString());
+  const outputTokenReserveQtyBN = BigNumber(outputTokenReserveQty);
+  const slippagePercentBN = BigNumber(slippagePercent);
 
-  if(inputTokenReserveQty === 0 || inputTokenReserveQty === null || inputTokenReserveQty === BigNumber(0)
-     || outputTokenReserveQty === 0 || outputTokenReserveQty === null || outputTokenReserveQty === BigNumber(0)) {
-    
+  if(inputTokenReserveQtyBN.isEqualTo(BigNumber(0)) || outputTokenReserveQtyBN.isEqualTo(BigNumber(0)) ) {
     throw new Error("Error:Empty pool");
-    
   }
   else
-    if(inputTokenAmount === 0 || inputTokenAmount === null || inputTokenAmount === BigNumber(0)) {
+    if(inputTokenAmountBN.isEqualTo(0)) {
       throw new Error("Divide by zero");
     }
   
-  const inputTokenAmountBN = BigNumber(inputTokenAmount);
-  const inputTokenReserveQtyBN = BigNumber(inputTokenReserveQty);
-  const outputTokenReserveQtyBN = BigNumber(outputTokenReserveQty);
-  const slippagePercentBN = BigNumber(slippagePercent);
+  
 
   const k = inputTokenReserveQtyBN.multipliedBy(outputTokenReserveQtyBN)
 
