dfe7e8cd7f6ba98065e07edb049db6d21c5326e4
SSDD <smalldutta@gmail.com>
2021-11-08 20:10:58 +0200

    adds priceimpact test

--- src/utils/mathLib.js
+++ src/utils/mathLib.js
@@ -2,42 +2,86 @@ const sdk = require("@elastic-dao/sdk");
 const BigNumber = require("bignumber.js");
 const {utils} = sdk;
 
+// TODO: add support for negative scenarios
 
 // calculates the min amount of output tokens given the slippage percent supplied
 const calculateOutputAmount = (inputTokenAmount, inputTokenReserveQty, outputTokenReserveQty, slippagePercent) => {
 
   // inputTokenReserveQty - X
   // logic: K = X*Y
-  // outputTokenAmount = ((X*Y)/ inputTokenAmount)* (1 - (slippagePercent/100))
+  // YDash = ((K)/ inputTokenAmount)* (1 - (slippagePercent/100))
+  // outputTokenAmount = Y - YDash (difference)
   // cleanse input to BN
   const inputTokenAmountBN = BigNumber(inputTokenAmount);
   const inputTokenReserveQtyBN = BigNumber(inputTokenReserveQty);
-  console.log('inputTokenReserveQtyBN.toString()', inputTokenReserveQtyBN.toString());
   const outputTokenReserveQtyBN = BigNumber(outputTokenReserveQty);
   const slippagePercentBN = BigNumber(slippagePercent);
+  console.log("calcOutputAmount inputs:",inputTokenAmountBN.toString(), inputTokenReserveQtyBN.toString(), outputTokenReserveQtyBN.toString());
 
   if(inputTokenReserveQtyBN.isEqualTo(BigNumber(0)) || outputTokenReserveQtyBN.isEqualTo(BigNumber(0)) ) {
     throw new Error("Error:Empty pool");
   }
   else
     if(inputTokenAmountBN.isEqualTo(0)) {
-      throw new Error("Divide by zero");
+      throw new Error("Error: Divide by zero");
     }
+
+  const k = inputTokenReserveQtyBN.multipliedBy(outputTokenReserveQtyBN);
+  const XDash = inputTokenReserveQtyBN.plus(inputTokenAmountBN)
+  const YDash = k.dividedBy(XDash).multipliedBy(BigNumber("1").minus(slippagePercentBN.dividedBy(BigNumber("100"))));
+  const deltaY = YDash.minus(outputTokenReserveQtyBN);
+  const outputTokenAmountBN = deltaY.absoluteValue();
   
-  
-
-  const k = inputTokenReserveQtyBN.multipliedBy(outputTokenReserveQtyBN)
-
-  const outputTokenAmount = inputTokenReserveQtyBN.multipliedBy(outputTokenReserveQtyBN).dividedBy(inputTokenAmountBN)
-                              .multipliedBy(BigNumber("1").minus(slippagePercentBN.dividedBy(BigNumber("100"))));
-                             
-
-  const outputTokenAmountBN = BigNumber(outputTokenAmount);   
-                            
+  console.log("calcoutput: outputBN: ", outputTokenAmountBN.toString())                              
   return outputTokenAmountBN;                                
 
 
 };
 
-module.exports = {calculateOutputAmount}
+// calculates the price impact (or % move in x/y)
+const calculatePriceImpact = (inputTokenAmount, inputTokenReserveQty, outputTokenReserveQty, slippagePercent) => {
+  // price impact is percentage difference in omega post trade
+  // in - x ; out - Y
+  // (X'/Y' - X/Y)/ X/Y
+  // cleanse input to BN
+  const inputTokenAmountBN = BigNumber(inputTokenAmount);
+  const inputTokenReserveQtyBN = BigNumber(inputTokenReserveQty);
+  const outputTokenReserveQtyBN = BigNumber(outputTokenReserveQty);
+  const slippagePercentBN = BigNumber(slippagePercent);
+  console.log("priceImpactfunc: ", inputTokenAmountBN.toString(), inputTokenReserveQtyBN.toString(), outputTokenReserveQtyBN.toString());
+
+  const priceOfXinY = calculateExchangeRate(inputTokenReserveQtyBN, outputTokenReserveQtyBN);
+  console.log("priceOfXinY:", priceOfXinY.toString());
+  const YRecieved = calculateOutputAmount(inputTokenAmountBN, inputTokenReserveQtyBN, outputTokenReserveQtyBN, slippagePercentBN);
+  console.log("Yreceived:", YRecieved.toString());
+  const YDash = YRecieved.minus(outputTokenReserveQtyBN).absoluteValue();
+  const XDash = inputTokenReserveQtyBN.plus(inputTokenAmount);
+  console.log("XDash:", XDash.toString());
+  console.log("YDash:", YDash.toString());
+  const priceOfXDashInYDash = calculateExchangeRate(XDash, YDash);
+  console.log("priceOfXDashInYDash:", priceOfXDashInYDash.toString());
+  const difference = priceOfXDashInYDash.minus(priceOfXinY);
+  console.log("diffrence:", difference.toString());
+  const priceImpact = (difference).dividedBy(priceOfXinY).absoluteValue();
+  return priceImpact;
+
+}
+
+// calculates the current exchange rate (x/y)
+const calculateExchangeRate = (inputTokenReserveQty, outputTokenReserveQty) => {
+  // cleanse input to BN
+  inputTokenReserveQtyBN = BigNumber(inputTokenReserveQty);
+  outputTokenReserveQtyBN = BigNumber(outputTokenReserveQty);
+
+  // if any one of them are zero return cause empty pool
+  if(inputTokenReserveQtyBN.isEqualTo(0) || outputTokenReserveQtyBN.isEqualTo(0)){
+    throw new Error("Error:Empty pool");
+  }
+
+  return inputTokenReserveQtyBN.dividedBy(outputTokenReserveQtyBN);
+
+}
+
+
+module.exports = {calculateOutputAmount, calculateExchangeRate, calculatePriceImpact}
 
--- test/utils/mathLib.test.mjs
+++ test/utils/mathLib.test.mjs
@@ -4,30 +4,60 @@ import mathLib from '../../src/utils/MathLib.js'
 import BigNumber from 'bignumber.js'
 
 const { assert } = chai;
-const { calculateOutputAmount } = mathLib;
+const { calculateOutputAmount, calculateExchangeRate, calculatePriceImpact } = mathLib;
 
 
 
 describe('MathLib', () => {
 
 
-  it.only('calculates using calculateOutputAmount correctly', async() => {
+  it('calculates using calculateOutputAmount correctly', async() => {
     
     // no slippage
-    const answer = calculateOutputAmount(1, 100, 100, 0);
-    assert.isTrue(answer.isEqualTo(BigNumber("10000")));
+    const answer = calculateOutputAmount(10, 500, 100, 0);
+    console.log('answer', answer.toString());
+    console.log('bn', BigNumber("1.96078431372549019608").toString());
+    assert.isTrue(answer.isEqualTo(BigNumber("1.96078431372549019608")));
 
     // 5 percent slippage
-    const slipageAnswer = calculateOutputAmount(1, 100, 100, 5);
-    assert.isTrue(slipageAnswer.isEqualTo(BigNumber("9500")))
+    const slippageAnswer = calculateOutputAmount(10, 500, 100, 5);
+    console.log(slippageAnswer.toString());
+    assert.isTrue(slippageAnswer.isEqualTo(BigNumber("6.862745098039215686276")));
 
     // empty reserves should return sane error
     assert.throws(() => calculateOutputAmount(1, 0, 100, 5), "Error:Empty pool")
     assert.throws(() => calculateOutputAmount(1, 100, 0, 5), "Error:Empty pool")
-    assert.throws(() => calculateOutputAmount(0, 100, 100, 5), "Divide by zero")
+
+    // incorrect trade amount
+    assert.throws(() => calculateOutputAmount(0, 100, 100, 5), "Error: Divide by zero")
 
   });
 
+  it('calculates the exchange rate using calculateExchangeRate correctly', async ()  => {
+
+    // bunch of sane values
+    const rate1 = calculateExchangeRate(100,100);
+    assert.isTrue(rate1.isEqualTo(BigNumber(1)));
+    const rate2 = calculateExchangeRate(600,10000);
+    assert.isTrue(rate2.isEqualTo(BigNumber(0.06)));
+
+    // null values - should err gracefully
+    assert.throws(() => calculateExchangeRate(0, 100), "Error:Empty pool");
+    assert.throws(() => calculateExchangeRate(100, 0), "Error:Empty pool");
+  });
+
+  it.only('calculates priceImpact correctly', async () => {
+    const priceImpact1 = calculatePriceImpact(10, 500, 100, 0);
+    console.log(priceImpact1.toString());
+    assert.isTrue(priceImpact1.isEqualTo(BigNumber(0.0404)));
+
+    const priceImpact2 = calculatePriceImpact(10, 500, 100, 5);
+    console.log(priceImpact2.toString());
+    assert.isTrue(priceImpact2.isEqualTo(BigNumber("0.09515789473684210526")));
+
+  });
+
+  
 
 
 
