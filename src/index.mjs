ee1f88938a979eb6955ff20d9e8997d3e2b68ccb
Dan Matthews <dan@cryptobuilders.io>
2022-03-10 12:36:59 +0100

    user signer's provider globally when available (#36)

--- src/index.mjs
+++ src/index.mjs
@@ -46,7 +46,7 @@ export const ErrorHandling = ErrorHandlingClass;
 export class SDK extends Subscribable {
   constructor({ account, customFetch, env, provider, signer, storageAdapter }) {
     super();
-    this.provider = provider || ethers.getDefaultProvider();
+    this._provider = provider || ethers.getDefaultProvider();
     this._contract = ({ address, abi }) => new ethers.Contract(address, abi);
     this._storageAdapter = storageAdapter || new LocalStorageAdapter();
     this.signer = signer;
@@ -115,6 +115,10 @@ export class SDK extends Subscribable {
     return this._fetch;
   }
 
+  get provider() {
+    return this.signer ? this.signer.provider : this._provider;
+  }
+
   get storageAdapter() {
     return this._storageAdapter;
   }
@@ -134,11 +138,11 @@ export class SDK extends Subscribable {
   }
 
   async changeSigner(signer) {
-    let address = signer.address;
-    if (!address && signer.getAddress) {
-      address = await signer.getAddress();
+    let newAccount = signer.address;
+    if (!newAccount && signer.getAddress) {
+      newAccount = await signer.getAddress();
     }
-    this.account = address;
+    this.account = newAccount;
     this.signer = signer;
     this.balanceOf(this.account);
     await this.setName();
