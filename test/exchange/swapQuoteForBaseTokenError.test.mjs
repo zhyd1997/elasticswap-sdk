9a9a3f7de71c8d36b5eefedc91920b253502d353
SSDD <0xSSDD@protonmail.com>
2022-08-18 12:48:13 +0300

    hardcodes tc info for test

--- test/exchange/swapQuoteForBaseTokenError.test.mjs
+++ test/exchange/swapQuoteForBaseTokenError.test.mjs
@@ -7,29 +7,43 @@ import { buildCoreObjects } from '../testHelpers.mjs';
 
 const { ethers, deployments } = hardhat;
 
-const alchemyProvider = new ethers.providers.AlchemyProvider(
-  'homestead',
-  '-GwTHTJar_yTzhNX_Kns-fSBfAN5N3h8',
-);
-
 describe('Swap test', () => {
-  it('checks if there is an error with the sdk', async () => {
-    const coreObjects = await buildCoreObjects(deployments, ethers.provider);
-    const { baseToken, quoteToken, elasticSwapSDK, sdk, toBigNumber } = coreObjects;
+  let coreObjects;
+  let accounts;
+  let exchange;
+  let snapshotId;
 
-    const accounts = await ethers.getSigners();
+  before(async () => {
+    coreObjects = await buildCoreObjects(deployments, ethers.provider);
+    accounts = await ethers.getSigners();
+  });
 
+  beforeEach(async () => {
+    const { baseToken, quoteToken, sdk } = coreObjects;
+
+    // save the state of the blockchain before the test
+    snapshotId = await sdk.provider.send('evm_snapshot');
+
+    // set the signer to the moneybags account
     await sdk.changeSigner(accounts[0]);
+
+    // deploy a new exchange
+    await sdk.exchangeFactory.createNewExchange(baseToken.address, quoteToken.address);
+    exchange = await sdk.exchangeFactory.exchange(baseToken.address, quoteToken.address);
+  });
+
+  afterEach(async () => {
+    const { sdk } = coreObjects;
+    // rollback to the state before the test to prevent pollution
+    await sdk.provider.send('evm_revert', [snapshotId]);
+  });
+
+  it('checks if there is an error with the sdk', async () => {
+    const { baseToken, quoteToken, elasticSwapSDK, sdk, toBigNumber } = coreObjects;
 
     const expiration = Math.round(new Date().getTime() / 1000 + 60 * 50);
     const liquidityProvider = accounts[1];
     const liquidityProviderInitialBalances = 1000000;
-
-    // deploy a new exchange
-    // in this local exchange, need to set its balances as what it was at block: 15304423
-    // 15304423 is one block before the block in which tx https://etherscan.io/tx/0xfe104eb24f1a006217f3346a6d3f33b940ec1c82e54a6df7a754c1ea31f1ce05 failed
-    await sdk.exchangeFactory.createNewExchange(baseToken.address, quoteToken.address);
-    const exchange = await sdk.exchangeFactory.exchange(baseToken.address, quoteToken.address);
 
     const exchangeInstance = new elasticSwapSDK.Exchange(
       sdk,
@@ -49,22 +63,15 @@ describe('Swap test', () => {
       liquidityProviderInitialBalances,
     );
 
-    // to get the required internalbalances at 15304423
-    const mainnetExchangeContract = new ethers.Contract(
-      '0x79274BF95e05f0e858ab78411f3eBe85909E4F76',
-      exchangeInstance.abi,
-      alchemyProvider,
-    );
-    const internalBalances = await mainnetExchangeContract.internalBalances({
-      blockTag: '0xE986E7',
-    });
-
     // send users (liquidity provider) base and quote tokens for easy accounting.
     await baseToken.transfer(liquidityProvider.address, liquidityProviderInitialBalances);
     await quoteToken.transfer(liquidityProvider.address, liquidityProviderInitialBalances);
 
-    const baseTokenQtyToAdd = toBigNumber(internalBalances.baseTokenReserveQty, 18);
-    const quoteTokenQtyToAdd = toBigNumber(internalBalances.quoteTokenReserveQty, 6);
+    // retrived based on tx https://etherscan.io/tx/0xfe104eb24f1a006217f3346a6d3f33b940ec1c82e54a6df7a754c1ea31f1ce05
+    const baseTokenQtyToAdd = toBigNumber('15246029054780205919247', 18);
+    const quoteTokenQtyToAdd = toBigNumber('80096085623', 6);
+    const quoteTokenQtyToSwap = toBigNumber('1000');
+    const minBaseTokenQtyToSwap = toBigNumber('188.414252404592205063');
 
     // exchange has the same amount of liquidity as the mainnet pool at 15304423
     await exchangeInstance.addLiquidity(
@@ -75,10 +82,6 @@ describe('Swap test', () => {
       liquidityProvider.address,
       expiration,
     );
-
-    // these numbers are based on user input in https://etherscan.io/tx/0xfe104eb24f1a006217f3346a6d3f33b940ec1c82e54a6df7a754c1ea31f1ce05
-    const quoteTokenQtyToSwap = toBigNumber('1000');
-    const minBaseTokenQtyToSwap = toBigNumber('188.414252404592205063');
 
     // now call swapQuoteTokenForBaseToken on the local exchange with the values passed into the failed tx
     try {
